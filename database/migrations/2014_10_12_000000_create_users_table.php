d21ea9da9b35afb0ce0918f72db5f7e9eb756e9e
tuanvm8 <tuanvm8>
2023-03-13 23:54:58 +0700

    update table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken()->nullable();
-            $table->forgot_token()->nullable();
+            $table->string('forgot_token')->nullable();
             $table->timestamps();
         });
     }
